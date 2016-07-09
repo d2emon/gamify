389afcc72b7ff92593337a64200e5cd3c6b13f2c
Dmitry Kutsenko <d2emonium@gmail.com>
2016-07-09 23:25:23 +0300

    Advice fixtures

---
+++ tests/codeception/unit/fixtures/AdviceFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace tests\unit\fixtures;
+
+use yii\test\ActiveFixture;
+
+class AdviceFixture extends ActiveFixture
+{
+     public $modelClass = 'app\modules\advice\models\Advice';
+}
