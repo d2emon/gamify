15ab8141995d68ffbc21f3ca27eae83029fc50c4
Dmitry Kutsenko <d2emonium@gmail.com>
2016-07-20 10:47:22 +0300

    Task fixture

---
+++ tests/codeception/unit/fixtures/TaskGroupFixture.php
@@ -0,0 +1,9 @@
+<?php
+namespace tests\unit\fixtures;
+
+use yii\test\ActiveFixture;
+
+class TaskGroupFixture extends ActiveFixture
+{
+     public $modelClass = 'app\modules\task\models\TaskGroup';
+}
